866b515a89e707cc0b3ea283671cbfc09b5c2e53
TasinTausif <tasintausif@gmail.com>
2025-11-25 01:46:08 +0600

    partial done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
+import { useEffect, useState } from "react";
+import ThemeBtn from "./Components/ThemeBtn";
+import ThemeCard from "./Components/ThemeCard";
+import { ThemeProvider } from "./context/theme";
+
 export default function App() {
+  const [themeMode, setThemeMode] = useState('light')
+
+  const lightTheme = () => setThemeMode("light")
+  const darkTheme  = () => setThemeMode("dark")
+
+  const currentMode = document.querySelector('html')
+  // Actual theme change
+  useEffect(() => {
+    currentMode.classList.remove("light", "dark")
+    currentMode.classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
 
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* Theme btn */}
-        </div>
-
-        <div className="w-full max-w-sm mx-auto">
-          {/* Theme card */}
+          <div className="w-full max-w-sm mx-auto">
+            <ThemeCard />
+          </div>
         </div>
       </div>
-    </div>
-
+    </ThemeProvider>
   )
 }
--- src/context/theme.js
+++ src/context/theme.js
@@ -1,5 +1,16 @@
-import { createContext } from "react"
+import { createContext, useContext } from "react"
 
+// Not necessarily we have to pass state values and state methods only in the context, we can pass any values and any functions too 
 export const ThemeContext = createContext({
     themeMode: "light",
+    lightTheme: () => {},
+    darkTheme: () => {}
 })
+
+// We'll use the provider ultimately, so passing data to Context here through the Provider
+export const ThemeProvider = ThemeContext.Provider
+
+// custom hook for the context, which is usually used
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
